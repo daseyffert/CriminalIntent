c89d13bad00d7a13a446fb36aed26d71a4074453
Daniel Seyffert <daseyffert@gmail.com>
2015-11-01 09:08:11 -0700

    Foundational Fragment created

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -10,6 +12,18 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        //Get Fragment Manager first so that can get fragment class
+        FragmentManager fm = getSupportFragmentManager();
+        //Ask fragment manager if fragment_container is in list
+        Fragment fragment = fm.findFragmentById(R.id.activity_crime_fragment_container);
+        //Checks if fragment manager found and returned fragment_container
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            //.beginTransaction() = creates and returns instance of FRAGMENT TRANSACTION
+            //.add().commit() = create and commit FRAGMENT TRANSACTION
+            fm.beginTransaction().add(R.id.activity_crime_fragment_container, fragment).commit();
+        }
     }
 
 
